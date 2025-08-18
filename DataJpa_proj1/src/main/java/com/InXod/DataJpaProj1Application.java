19ead8991da51b35c2a598641c366cf599bdf300
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-18 18:18:59 +0530

    feat(main) : initial commit

---
+++ DataJpa_proj1/src/main/java/com/InXod/DataJpaProj1Application.java
@@ -0,0 +1,13 @@
+package com.InXod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaProj1Application.class, args);
+	}
+
+}
