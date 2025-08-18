6e798bae1b8950a086f96921377b45ce7e2af531
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-18 18:21:26 +0530

    initial commit

---
+++ DataJpa_proj1/src/test/java/com/InXod/DataJpaProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InXod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
