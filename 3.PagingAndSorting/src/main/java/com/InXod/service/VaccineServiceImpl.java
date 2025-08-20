b1b277146b38a8f0f87b073e9d89393b506e1506
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-20 14:09:42 +0530

    feat(service) : implementation of vaccine service interface : business logic for sorting vaccine

--- 3.PagingAndSorting/src/main/java/com/InXod/service/VaccineServiceImpl.java
+++ 3.PagingAndSorting/src/main/java/com/InXod/service/VaccineServiceImpl.java
@@ -1,8 +1,12 @@
 package com.InXod.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.InXod.model.Vaccine;
 import com.InXod.repository.IVaccineRepository;
 
 @Service
@@ -13,6 +17,19 @@ public class VaccineServiceImpl implements IVaccineService {
 	@Autowired
 	public void setVaccRepo(IVaccineRepository vaccRepo) {
 		this.vaccRepo = vaccRepo;
+	}
+
+	@Override
+	public Iterable<Vaccine> fetchVaccineInfoBySorting(boolean status, String... properties) {
+		
+		// Create Sort object based on status and properties
+	    // If status is true -> ASC (Ascending) order
+	    // If status is false -> DESC (Descending) order
+		// using ternary operator  / (condition ? valueIfTrue : valueIfFalse)
+		Sort sort = Sort.by(status?Direction.ASC : Direction.DESC, properties);
+		
+		// Fetch all vaccine records from repository with the specified sorting
+		return vaccRepo.findAll(sort);
 	}
 	
 	
