e3b48d3d3831b6a1889bd75b2f9bc969374be6d8
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-20 12:19:08 +0530

    feat(service) : implementation of vaccine service interface

--- DataJpa_proj2/src/main/java/com/InXod/service/VaccineServiceImpl.java
+++ DataJpa_proj2/src/main/java/com/InXod/service/VaccineServiceImpl.java
@@ -1,5 +1,8 @@
 package com.InXod.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +27,78 @@ public class VaccineServiceImpl implements IVaccineService {
 	public Iterable<Vaccine> registerMultipleVaccine(Iterable<Vaccine> vaccine) {
 		
 		return vaccRepo.saveAll(vaccine);
+	}
+
+	@Override
+	public Long getCount() {
+	
+		return vaccRepo.count();
+	}
+
+	@Override
+	public Boolean checkAvailibility(Integer id) {
+
+		return vaccRepo.existsById(id);
+	}
+
+	@Override
+	public Iterable<Vaccine> fetchAllVaccine() {
+		
+		return vaccRepo.findAll();
+	}
+
+	@Override
+	public Iterable<Vaccine> fetchAllVaccineByIds(Iterable<Integer> ids) {
+		
+		return vaccRepo.findAllById(ids);
+	}
+
+	@Override
+	public Optional<Vaccine> fetchVaccineById(Integer id) {
+		
+		return vaccRepo.findById(id);
+	}
+
+	@Override
+	public String removeVaccineById(Integer id) {
+		
+		Optional<Vaccine> optional = vaccRepo.findById(id);
+		if(optional.isPresent()) {
+			vaccRepo.deleteById(id);
+			return "Vaccine record with id "+id+" is deleted";
+		}
+		return "There is no Vaccine with id "+id+" to delete";
+	}
+
+	@Override
+	public String removeVaccineByObj(Vaccine obj) {
+		Integer id = obj.getVaccineId();
+		Optional<Vaccine> optional = vaccRepo.findById(id);
+		if(optional.isPresent()) {
+			vaccRepo.deleteById(id);
+			return "Vaccine record with id "+id+" is deleted";
+		}
+		return "There is no Vaccine with id "+id+" to delete";
+	}
+
+	@Override
+	public String removeVaccineByIds(List<Integer> ids) {
+		
+		List<Vaccine> vaccine = (List<Vaccine>)vaccRepo.findAllById(ids);
+		int count1 = ids.size();
+		int count2 = vaccine.size();
+		
+//		if(count1 == count2) {
+//			vaccRepo.deleteAllById(ids);
+//			return "Vaccine are deleted from records";
+//		}
+		
+		if(count2>0) {
+			vaccRepo.deleteAllById(ids);
+			return "Vaccine are deleted from records";
+		}
+		
+		return "Failed to delete vaccine info";
 	}
 
 	
