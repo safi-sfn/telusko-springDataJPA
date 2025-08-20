12f7683e94a778b86b5c8036ebb750e559705e5d
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-20 12:17:16 +0530

    feat(service) : all CrudRepository method define

--- DataJpa_proj2/src/main/java/com/InXod/service/IVaccineService.java
+++ DataJpa_proj2/src/main/java/com/InXod/service/IVaccineService.java
@@ -1,5 +1,9 @@
 package com.InXod.service;
 
+
+import java.util.List;
+import java.util.Optional;
+
 import com.InXod.model.Vaccine;
 
 public interface IVaccineService {
@@ -7,4 +11,19 @@ public interface IVaccineService {
 	public String registerVaccineDetails(Vaccine vaccine);
 	
 	Iterable<Vaccine> registerMultipleVaccine(Iterable<Vaccine> vaccine);
+	
+	public Long getCount();
+	
+	Boolean checkAvailibility(Integer id);
+	
+	Iterable<Vaccine> fetchAllVaccine();
+	
+	Iterable<Vaccine> fetchAllVaccineByIds(Iterable<Integer> ids);
+	
+	Optional<Vaccine> fetchVaccineById(Integer id);
+	
+	String removeVaccineById(Integer id);
+	String removeVaccineByObj(Vaccine obj);
+	String removeVaccineByIds(List<Integer> ids);
+
 }
