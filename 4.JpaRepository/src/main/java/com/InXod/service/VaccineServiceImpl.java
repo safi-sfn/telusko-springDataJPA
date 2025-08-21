043cc72f8059b556146f53fef1e377075c9580ff
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-21 17:30:22 +0530

    feat(service) : implements vaccine service and using jpaRepository method

--- 4.JpaRepository/src/main/java/com/InXod/service/VaccineServiceImpl.java
+++ 4.JpaRepository/src/main/java/com/InXod/service/VaccineServiceImpl.java
@@ -4,9 +4,14 @@ package com.InXod.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.InXod.model.Vaccine;
 import com.InXod.repository.IVaccineRepository;
 
 @Service
@@ -19,6 +24,30 @@ public class VaccineServiceImpl implements IVaccineService {
 		this.vaccRepo = vaccRepo;
 	}
 
-	
+	@Override
+	public Vaccine fetchVaccineById(Integer id) {
+		
+		Vaccine vcc = vaccRepo.getReferenceById(id);
+		return vcc;
+	}
+
+	@Override
+	public List<Vaccine> fetchByGivenVaccineInfo(Vaccine vacc) {
+		
+		Example<Vaccine> example = Example.of(vacc);
+		return vaccRepo.findAll(example);
 		
 	}
+
+	@Override
+	public String removeVaccineByGivenId(Iterable<Integer> id) {
+		List<Vaccine> vaccines = vaccRepo.findAllById(id);
+		if(vaccines.size() != 0) {	
+			vaccRepo.deleteAllByIdInBatch(id);
+			return "Vaccine record is deleted for given ids";
+		}
+		return "unable to delete record";
+	}
+
+		
+}
