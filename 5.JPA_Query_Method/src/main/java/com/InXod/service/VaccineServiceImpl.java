fdf8ffebd86ccc3c2ddb9233724bfd9b77a7e42c
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-21 20:52:34 +0530

    feat(service) : implements vaccine service interface and create vaccineRepo bean from setter method property with the help of @Autowired

--- 5.JPA_Query_Method/src/main/java/com/InXod/service/VaccineServiceImpl.java
+++ 5.JPA_Query_Method/src/main/java/com/InXod/service/VaccineServiceImpl.java
@@ -1,9 +1,13 @@
 package com.InXod.service;
 
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.InXod.model.Vaccine;
 import com.InXod.repository.IVaccineRepository;
 
 @Service
@@ -14,6 +18,49 @@ public class VaccineServiceImpl implements IVaccineService {
 	@Autowired
 	public void setVaccRepo(IVaccineRepository vaccRepo) {
 		this.vaccRepo = vaccRepo;
+	}
+
+	@Override
+	public List<Vaccine> searchByvaccineCost(Double vaccineCost) {
+		
+		return vaccRepo.findByvaccineCost(vaccineCost);
+	}
+
+	@Override
+	public List<Vaccine> searchByvaccineCostIs(Double vaccineCost) {
+		
+		return vaccRepo.findByvaccineCostIs(vaccineCost);
+	}
+
+	@Override
+	public List<Vaccine> searchByvaccineCostEquals(Double vaccineCost) {
+		
+		return vaccRepo.findByvaccineCostEquals(vaccineCost);
+	}
+
+	@Override
+	public List<Vaccine> searchByvaccineCostLessThanEqual(Double vaccineCost) {
+	
+		return vaccRepo.findByvaccineCostLessThanEqual(vaccineCost);
+	}
+
+	@Override
+	public List<Vaccine> searchByvaccineCostBetween(Double minVaccineCost, Double maxVaccineCost) {
+		// TODO Auto-generated method stub
+		return vaccRepo.findByvaccineCostBetween(minVaccineCost,maxVaccineCost);
+	}
+
+	@Override
+	public List<Vaccine> searchTop7ByOrderByVaccineCostDesc() {
+		
+		return vaccRepo.findTop7ByOrderByVaccineCostDesc();
+	}
+
+	@Override
+	public List<Vaccine> searchByVaccineNameInAndCostBetween(Collection<String> names, Double minVaccineCost,
+			Double maxVaccineCost) {
+		
+		return vaccRepo.findByVaccineNameInAndVaccineCostBetween(names, minVaccineCost, maxVaccineCost);
 	}
 
 		
