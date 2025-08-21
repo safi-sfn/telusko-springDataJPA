dee0f36e78a62b838de14058fa45d534dea6021f
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-21 20:49:38 +0530

    feat(main) : creating vaccine service bean and fetching data from DB

--- 5.JPA_Query_Method/src/main/java/com/InXod/JpaQueryMethodApplication.java
+++ 5.JPA_Query_Method/src/main/java/com/InXod/JpaQueryMethodApplication.java
@@ -1,15 +1,13 @@
 package com.InXod;
 
 
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.InXod.model.Vaccine;
 import com.InXod.service.VaccineServiceImpl;
 
 @SpringBootApplication
@@ -19,6 +17,20 @@ public class JpaQueryMethodApplication {
 		ConfigurableApplicationContext container = SpringApplication.run(JpaQueryMethodApplication.class, args);
 
 		VaccineServiceImpl vaccService = container.getBean(VaccineServiceImpl.class);
-
+		
+//		vaccService.searchByvaccineCost(2500.0).forEach(v->System.out.println(v));
+//		vaccService.searchByvaccineCostIs(2689.5).forEach(v->System.out.println(v));
+//		vaccService.searchByvaccineCostEquals(3599.0).forEach(v->System.out.println(v));
+		
+//		vaccService.searchByvaccineCostLessThanEqual(2500.0).forEach(v->System.out.println(v));
+//		vaccService.searchByvaccineCostBetween(2670.0,3600.0).forEach(v->System.out.println(v));
+		
+//		vaccService.searchTop7ByOrderByVaccineCostDesc().forEach(v->System.out.println(v));
+		
+		Collection<String> names = new HashSet<>();
+		names.add("Polio (IPV/OPV)");
+//		names.add("Moderna");
+		names.add("Co-vaccine");
+		vaccService.searchByVaccineNameInAndCostBetween(names, 2400.0, 2700.0).forEach(v->System.out.println(v));
 	}
 }
