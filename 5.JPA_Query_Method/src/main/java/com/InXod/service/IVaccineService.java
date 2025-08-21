e0b613d8845ffc62e2963b70dd2c4d0da7e34776
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-21 20:48:32 +0530

    feat(service) : defining custom search method

--- 5.JPA_Query_Method/src/main/java/com/InXod/service/IVaccineService.java
+++ 5.JPA_Query_Method/src/main/java/com/InXod/service/IVaccineService.java
@@ -1,7 +1,23 @@
 package com.InXod.service;
 
+import java.util.Collection;
+import java.util.List;
 
+import com.InXod.model.Vaccine;
 
 public interface IVaccineService {
+
+	public List<Vaccine> searchByvaccineCost(Double vaccineCost);
+
+	public List<Vaccine> searchByvaccineCostIs(Double vaccineCost);
+
+	public List<Vaccine> searchByvaccineCostEquals(Double vaccineCost);
 	
+    public List<Vaccine> searchByvaccineCostLessThanEqual(Double vaccineCost);
+	
+	public List<Vaccine> searchByvaccineCostBetween(Double minVaccineCost, Double maxVaccineCost);
+	
+	public List<Vaccine> searchTop7ByOrderByVaccineCostDesc();
+	
+	  List<Vaccine> searchByVaccineNameInAndCostBetween(Collection<String> names ,Double minVaccineCost, Double maxVaccineCost);
 }
