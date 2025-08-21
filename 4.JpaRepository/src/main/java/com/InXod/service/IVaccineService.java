e4ddd9491096ad6ef924b6897a38d6e58f7adf34
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-21 17:28:57 +0530

    feat(service) : adding three method

--- 4.JpaRepository/src/main/java/com/InXod/service/IVaccineService.java
+++ 4.JpaRepository/src/main/java/com/InXod/service/IVaccineService.java
@@ -1,6 +1,14 @@
 package com.InXod.service;
 
+import java.util.List;
+
+import com.InXod.model.Vaccine;
 
 public interface IVaccineService {
 	
+	Vaccine fetchVaccineById(Integer id);
+	
+	List<Vaccine> fetchByGivenVaccineInfo(Vaccine vacc);
+	
+	String removeVaccineByGivenId(Iterable<Integer> id);
 }
