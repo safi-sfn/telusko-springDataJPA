51c591694875b66aeed80f7206360aaff2e805d3
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-21 17:29:32 +0530

    feat(main) : Main method

--- 4.JpaRepository/src/main/java/com/InXod/JpaRepositoryApplication.java
+++ 4.JpaRepository/src/main/java/com/InXod/JpaRepositoryApplication.java
@@ -2,10 +2,14 @@ package com.InXod;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.InXod.model.Vaccine;
 import com.InXod.service.VaccineServiceImpl;
 
 @SpringBootApplication
@@ -15,7 +19,19 @@ public class JpaRepositoryApplication {
 		ConfigurableApplicationContext container = SpringApplication.run(JpaRepositoryApplication.class, args);
 
 		VaccineServiceImpl vaccService = container.getBean(VaccineServiceImpl.class);
+		
+//		Vaccine vc =vaccService.fetchVaccineById(53);
+//		System.out.println(vc);
+		
 
-
+//		vaccService.fetchByGivenVaccineInfo(new Vaccine("Rotavac","Bharat Biotech",2500.0))
+//		.forEach(v->System.out.println(v));
+		
+		List<Integer> ids = new ArrayList<>();
+		ids.add(104);
+		ids.add(53);
+		
+		String status = vaccService.removeVaccineByGivenId(ids);
+		System.out.println(status);
 	}
 }
