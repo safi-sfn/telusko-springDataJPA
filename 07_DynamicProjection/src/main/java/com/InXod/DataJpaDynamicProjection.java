ac78aae75d0c8f442dda7bbd92f2971ee36b61e5
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-23 12:44:13 +0530

    feat(main) : Main class

--- 07_DynamicProjection/src/main/java/com/InXod/DataJpaDynamicProjection.java
+++ 07_DynamicProjection/src/main/java/com/InXod/DataJpaDynamicProjection.java
@@ -1,12 +1,13 @@
 package com.InXod;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.InXod.service.VaccineServiceImpl;
+import com.InXod.view.ResultView1;
+import com.InXod.view.ResultView2;
+import com.InXod.view.ResultView3;
 
 @SpringBootApplication
 public class DataJpaDynamicProjection {
@@ -16,6 +17,16 @@ public class DataJpaDynamicProjection {
 
 		VaccineServiceImpl vaccService = container.getBean(VaccineServiceImpl.class);
 
+		vaccService.searchByVaccineCostLessThan(2600.0, ResultView1.class)
+		.forEach(v->System.out.println("Vaccine Name : "+v.getVaccineName()));
+		
+		vaccService.searchByVaccineCostLessThan(2600.0, ResultView2.class)
+		.forEach( v -> {
+			System.out.println("Vaccine Company : "+v.getVaccineCompany());
+			System.out.println("Vaccine Cost    : "+v.getVaccineCost());
+		});
+		vaccService.searchByVaccineCostLessThan(2400.0, ResultView3.class)
+		.forEach(v->System.out.println(v));
 
 	}
 }
