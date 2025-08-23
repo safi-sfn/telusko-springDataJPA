6d610561dd489983f47af8529dc8f81a41dade5e
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-23 11:17:04 +0530

    feat(service) : Interface and its impl for static projection

--- 06_StaticProjection/src/main/java/com/InXod/service/IVaccineService.java
+++ 06_StaticProjection/src/main/java/com/InXod/service/IVaccineService.java
@@ -1,7 +1,14 @@
 package com.InXod.service;
 
+import java.util.List;
+
+
+import com.InXod.view.ResultView;
+import com.InXod.view.ResultView2;
 
 public interface IVaccineService {
 
+	public List<ResultView> searchByVaccineCostLessThan(Double vaccineCost);
 	
+	List<ResultView2> searchFirst5ByVaccineCompanyOrderByVaccineNameAsc(String company);
 }
--- 06_StaticProjection/src/main/java/com/InXod/service/VaccineServiceImpl.java
+++ 06_StaticProjection/src/main/java/com/InXod/service/VaccineServiceImpl.java
@@ -1,9 +1,15 @@
 package com.InXod.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
 import com.InXod.repository.IVaccineRepository;
+import com.InXod.view.ResultView;
+import com.InXod.view.ResultView2;
 
 @Service
 public class VaccineServiceImpl implements IVaccineService {
@@ -14,6 +20,20 @@ public class VaccineServiceImpl implements IVaccineService {
 	public void setVaccRepo(IVaccineRepository vaccRepo) {
 		this.vaccRepo = vaccRepo;
 	}
+
+	@Override
+	public List<ResultView> searchByVaccineCostLessThan(Double vaccineCost) {
+		  return vaccRepo.findByVaccineCostLessThan(vaccineCost);
+		
+	}
+
+	@Override
+	public List<ResultView2> searchFirst5ByVaccineCompanyOrderByVaccineNameAsc(String company) {
+		
+		return vaccRepo.findFirst5ByVaccineCompanyOrderByVaccineNameAsc(company);
+	}
+
+	
 
 
 		
