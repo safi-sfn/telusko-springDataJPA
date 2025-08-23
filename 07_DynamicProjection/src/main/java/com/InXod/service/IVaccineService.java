4aaf72d1d7bfea3d03c61a5e0e87a5fd70fd6cde
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-23 12:43:33 +0530

    feat(service) : Interface and its Impl for dynamic projection

--- 07_DynamicProjection/src/main/java/com/InXod/service/IVaccineService.java
+++ 07_DynamicProjection/src/main/java/com/InXod/service/IVaccineService.java
@@ -1,5 +1,10 @@
 package com.InXod.service;
 
+import java.util.List;
+
+import com.InXod.view.View;
+
 public interface IVaccineService {
 
+	public <T extends View> List<T> searchByVaccineCostLessThan(Double vaccCost, Class<T> clss);
 }
--- 07_DynamicProjection/src/main/java/com/InXod/service/VaccineServiceImpl.java
+++ 07_DynamicProjection/src/main/java/com/InXod/service/VaccineServiceImpl.java
@@ -1,9 +1,12 @@
 package com.InXod.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.InXod.repository.IVaccineRepository;
+import com.InXod.view.View;
 
 
 @Service
@@ -14,6 +17,11 @@ public class VaccineServiceImpl implements IVaccineService {
 	@Autowired
 	public void setVaccRepo(IVaccineRepository vaccRepo) {
 		this.vaccRepo = vaccRepo;
+	}
+
+	@Override
+	public <T extends View> List<T> searchByVaccineCostLessThan(Double vaccCost,  Class<T> clss) {
+		return vaccRepo.findByVaccineCostLessThan(vaccCost, clss);
 	}
 		
 }
