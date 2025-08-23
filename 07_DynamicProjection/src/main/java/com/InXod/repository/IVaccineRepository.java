cde69cf1fc4247af4ce8183e828e4d2ab1132d38
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-23 12:46:41 +0530

    feat(repo) : defining custom finder for dynamic projection with class type "T" its dynamic projection

--- 07_DynamicProjection/src/main/java/com/InXod/repository/IVaccineRepository.java
+++ 07_DynamicProjection/src/main/java/com/InXod/repository/IVaccineRepository.java
@@ -1,10 +1,16 @@
 package com.InXod.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.InXod.model.Vaccine;
+import com.InXod.view.View;
 
 
 public interface IVaccineRepository extends JpaRepository<Vaccine, Integer> {
-
+	
+	// Dynamic projection concept
+	
+	public <T extends View> List<T> findByVaccineCostLessThan(Double vaccCost, Class<T> clss);
 }
