2e85eb53405c28a550b18af9a0eab61c8b1a1f03
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-23 11:16:26 +0530

    feat(util) : Marker interface and specific column view interface

--- 06_StaticProjection/src/main/java/com/InXod/view/ResultView.java
+++ 06_StaticProjection/src/main/java/com/InXod/view/ResultView.java
@@ -1,5 +1,7 @@
 package com.InXod.view;
 
 public interface ResultView extends View {
-
+	
+	public String getVaccineName();
+	public String getVaccineCompany();
 }
---
+++ 06_StaticProjection/src/main/java/com/InXod/view/ResultView2.java
@@ -0,0 +1,5 @@
+package com.InXod.view;
+
+public interface ResultView2 extends View {
+		public String getVaccineName();
+}
