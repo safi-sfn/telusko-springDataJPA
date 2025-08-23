20fef1b9c3377938d808be6a804d476adc1b2832
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-23 11:18:15 +0530

    feat(repo) : defining custom finder for static projection

--- 06_StaticProjection/src/main/java/com/InXod/repository/IVaccineRepository.java
+++ 06_StaticProjection/src/main/java/com/InXod/repository/IVaccineRepository.java
@@ -1,12 +1,16 @@
 package com.InXod.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.InXod.model.Vaccine;
+import com.InXod.view.ResultView;
+import com.InXod.view.ResultView2;
 
 public interface IVaccineRepository extends JpaRepository<Vaccine, Integer> {
 
+	List<ResultView> findByVaccineCostLessThan(Double vaccineCost);
 	
-	
-	
+	List<ResultView2> findFirst5ByVaccineCompanyOrderByVaccineNameAsc(String company);
 }
