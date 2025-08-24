ce2fb60d86dc8e25579000bbf3e8bbb4b9a4f36d
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-24 09:44:30 +0530

    feat(main) : main class

--- 08_CustomQuery/src/main/java/com/InXod/DataJpaCustomQuery.java
+++ 08_CustomQuery/src/main/java/com/InXod/DataJpaCustomQuery.java
@@ -17,11 +17,27 @@ public class DataJpaCustomQuery {
 
 		VaccineServiceImpl vaccService = container.getBean(VaccineServiceImpl.class);
 		
-		vaccService.fetchByVaccineCompany("Bharat Biotech").forEach(v->System.out.println(v));
+//		vaccService.fetchByVaccineCompany("Bharat Biotech").forEach(v->System.out.println(v));
+//		
+//		System.out.println("*******************************");
+//		
+//		vaccService.fetchByVaccineCompanies("AstraZenica", "Moderna").forEach(v->System.out.println(v));
 		
-		System.out.println("*******************************");
+//		vaccService.fetchByVaccineCost(2478.9, 2890.7).forEach(v->System.out.println(v));
 		
-		vaccService.fetchByVaccineCompanies("AstraZenica", "Moderna").forEach(v->System.out.println(v));
+//		int rowAffected = vaccService.updateVaccineCostByVaccineName(2245.6, "Moderna");
+//		System.out.println(rowAffected);
+		
+//		int rowAffected = vaccService.deleteTheVaccineByVaccineName("Polio (IPV/OPV)");
+//		System.out.println("Deleted success : "+rowAffected);
+		
+		int status = vaccService.insertVaccineDetails(5, "Ebula", "Crex", 2350.9);
+		
+		if(status==1) {
+			System.out.println("Insertion Success!");
+		}else {
+			System.out.println("Insertion Failure!");
+		}
 
 	}
 }
