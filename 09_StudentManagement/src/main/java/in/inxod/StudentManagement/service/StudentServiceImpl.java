4aa1dff960102d3ebb2aeff840991ac5306bf812
safi-sfn <mdsafiruddin24@gmail.com>
2025-08-31 15:10:42 +0530

    feat(service) : implementation for service interface and apply CRUD operation

---
+++ 09_StudentManagement/src/main/java/in/inxod/StudentManagement/service/StudentServiceImpl.java
@@ -0,0 +1,111 @@
+package in.inxod.StudentManagement.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.inxod.StudentManagement.model.Students;
+import in.inxod.StudentManagement.repository.StudentRepository;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+	
+	StudentRepository repo;
+	
+	@Autowired
+	public StudentServiceImpl(StudentRepository repo) {
+		super();
+		this.repo = repo;
+	}
+
+	@Override
+	public List<Students> addStudent(List<Students> students) {
+	        return (List<Students>)repo.saveAll(students);
+
+	}
+
+	@Override
+	public String updateStudentDetails(Students student) {
+		Optional<Students> optional = repo.findById(student.getStudentId());
+		
+		if(optional.isPresent()) {
+			Students present = optional.get();
+			present.setStudentName(student.getStudentName());
+			present.setStudentAge(student.getStudentAge());
+			present.setStudentCity(student.getStudentCity());
+			present.setCourse(student.getCourse());
+			 repo.save(present);
+			 
+			 return "Student updated successfully with ID: " + present.getStudentId();
+			
+		}else {
+			return "Student not found with ID: " + student.getStudentId();
+		}
+		
+	}
+
+	
+	@Override
+	public Optional<Students> findByStudentId(Integer id) {
+			Optional<Students> optional = repo.findById(id);
+		return optional;
+	}
+
+	
+	
+	@Override
+	public String deleteStudentById(Integer id) {
+		Optional<Students> optional = repo.findById(id);
+		if(optional.isPresent()) {
+			repo.deleteById(id);
+			return "Student Deleted Successfully with ID : "+id;
+		}
+		return "Student is Not Present for Delete with this ID : "+id;
+	}
+
+	@Override
+	public String deleteStudentWithMultipleIds(List<Integer> id) {
+		List<Students> list = (List<Students>)repo.findAllById(id);
+		int totalId = id.size();
+		int totalStudent = list.size();
+		
+//		if(totalId == totalStudent) {
+//			repo.deleteAllById(id);
+//			return "Students Deleted Successfully";
+//		}
+		
+		if(totalStudent>0) {
+			repo.deleteAllById(id);
+			return "Students Deleted Successfully";
+		}
+		return "Deletion failure";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
