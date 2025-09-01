7e853c46a73d7b7431b3d06914f9660f11493eff
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-01 21:42:49 +0530

    feat(service) : defining  method for CRUD operation

--- 14_HospitalPatientRecords/src/main/java/in/inxod/hospitalPatientRecords/service/PatientService.java
+++ 14_HospitalPatientRecords/src/main/java/in/inxod/hospitalPatientRecords/service/PatientService.java
@@ -1,13 +1,47 @@
 package in.inxod.hospitalPatientRecords.service;
 
+import java.time.LocalDate;
 import java.util.List;
+
+
 
 import in.inxod.hospitalPatientRecords.model.Patient;
 
 public interface PatientService {
 
-	
+	//Add patient
 	List<Patient> addMultiplePatient(List<Patient> patients);
 	
-    List<Patient> findByDisease(String disease);
+	//Get patient by disease
+    List<Patient> getPatientByDisease(String disease);
+    List<Patient> fetchPatientsByDoctor(String doctor);
+    
+    //Update doctor name
+    int updateDoctorNameByPatientId(String newDoctor, Integer id);
+    
+    
+    // Delete patient  
+    int deletePatientsById(Integer id);
+    
+    // After
+    List<Patient> getByAdmittedDateAfter(LocalDate date);
+    
+    //Before
+    List<Patient> getByAdmittedDateBefore(LocalDate date);
+    
+    // between
+    List<Patient> getByAdmittedDateBetween(LocalDate start, LocalDate end);
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
