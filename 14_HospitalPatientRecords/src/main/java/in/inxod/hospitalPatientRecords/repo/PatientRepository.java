3c834e556a5b046795430820228d59ae8ba72386
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-01 21:47:23 +0530

    feat(repo) : define a custom finder and define a JPQL query for custom query method

--- 14_HospitalPatientRecords/src/main/java/in/inxod/hospitalPatientRecords/repo/PatientRepository.java
+++ 14_HospitalPatientRecords/src/main/java/in/inxod/hospitalPatientRecords/repo/PatientRepository.java
@@ -18,33 +18,33 @@ public interface PatientRepository extends JpaRepository<Patient, Integer> {
 	
 	// custom finder method
 
+	
+	List<Patient> findByDisease(String disease);
+	
     // Patients admitted after, before and between two dates 
-	//  after specific date
     List<Patient> findByAdmittedDateAfter(LocalDate date);
-    //  before specific date
     List<Patient> findByAdmittedDateBefore(LocalDate date);
-    //  between two dates
     List<Patient> findByAdmittedDateBetween(LocalDate start, LocalDate end);
 
     
     // JPQL @Query
 
-    // 2. Specific doctor ke under admitted patients
+    // Specific doctor ke under admitted patients
     @Query("FROM Patient WHERE doctorName = :doctor")
-    List<Patient> getPatientsByDoctor(@Param("doctor") String doctor);
+    List<Patient> getPatientsByDoctorName(@Param("doctor") String doctor);
 
 
-    // 4. Update doctor name for a patient
+    //  Update doctor name for a patient
     @Transactional
     @Modifying
     @Query("UPDATE Patient SET doctorName = :newDoctor WHERE patientId = :id")
     int updateDoctorByPatientId(@Param("newDoctor") String newDoctor, @Param("id") Integer id);
 
-    // 5. Delete all patients by disease
+    // Delete  patient by id
     @Transactional
     @Modifying
-    @Query("DELETE FROM Patient WHERE disease = :disease")
-    int deletePatientsByDisease(@Param("disease") String disease);
+    @Query("DELETE FROM Patient WHERE patientId = :id")
+    int deletePatientsByPatientId(@Param("id") Integer id);
     
     
 }
