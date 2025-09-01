cd0ad662f8cbf30451a5d5972d2e015e244bde3a
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-01 16:25:36 +0530

    initial commit

---
+++ 13_CourseEnrollment/src/test/java/in/inxod/courseEnrollment/CourseEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.courseEnrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
