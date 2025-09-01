249e591098f3f6d6bc0a5a4ef9e08e0451427dde
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-01 09:40:59 +0530

    initial commit

---
+++ 11_EmployeeManagement/src/test/java/in/inxod/employeeManagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.employeeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
