32a16fce1b3b9e8fed5da2a97c4fbd0069480e35
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-01 17:41:08 +0530

    initial commit

---
+++ 14_HospitalPatientRecords/src/test/java/in/inxod/hospitalPatientRecords/HospitalPatientRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.hospitalPatientRecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalPatientRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
