711abe3f2ac2fb84c480796912e6f3c7d40ad6b7
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-01 21:45:58 +0530

    feat(service) : Implementation for defining method for CRUD operation - business logic

--- 14_HospitalPatientRecords/src/main/java/in/inxod/hospitalPatientRecords/service/PatientServiceImpl.java
+++ 14_HospitalPatientRecords/src/main/java/in/inxod/hospitalPatientRecords/service/PatientServiceImpl.java
@@ -1,5 +1,6 @@
 package in.inxod.hospitalPatientRecords.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ public class PatientServiceImpl implements PatientService {
 	@Autowired
 	private PatientRepository repo;
 	
-	
+	// ============Create Patients==========
 	@Override
 	public List<Patient> addMultiplePatient(List<Patient> patients) {
 		
@@ -25,11 +26,82 @@ public class PatientServiceImpl implements PatientService {
 		
 		return allPatient;
 	}
-
+	
+	
+	// ============Retrieve Patients==========
 	@Override
-	public List<Patient> findByDisease(String disease) {
+	public List<Patient> getPatientByDisease(String disease) {
 		// TODO Auto-generated method stub
-		return null;
+		List<Patient> patient = repo.findByDisease(disease);
+		patient.forEach(p->
+					System.out.println(p.getName()+" | "+p.getDoctorName()+" | "+p.getAdmittedDate())
+				);
+		return patient;
+	}
+
+	
+	// ============Update Patients==========
+	@Override
+	public int updateDoctorNameByPatientId(String newDoctor, Integer id) {
+		
+		return repo.updateDoctorByPatientId(newDoctor, id);
+	}
+
+	
+	// ============Delete Patients==========
+	@Override
+	public int deletePatientsById(Integer id) {
+		int rowAffected = repo.deletePatientsByPatientId(id);
+		if(rowAffected == 1) {
+			System.out.println("Patient deleted");
+		}else {
+			System.out.println("patient Not Found");
+		}
+		return rowAffected;
+	}
+
+	// ============Retrieve Patients==========
+	@Override
+	public List<Patient> fetchPatientsByDoctor(String doctor) {
+		List<Patient> patient = repo.getPatientsByDoctorName(doctor);
+		patient.forEach(p->
+					System.out.println(p.getName()+" | "+p.getDisease())
+				);
+		
+		return patient;
+	}
+
+	// ============Retrieve Patients==========
+	@Override
+	public List<Patient> getByAdmittedDateAfter(LocalDate date) {
+			
+		List<Patient> patient = repo.findByAdmittedDateAfter(date);
+		patient.forEach(p->
+		System.out.println(p.getName()+" | "+p.getDisease()+" | "+p.getDoctorName())
+	);
+		return patient;
+	}
+
+	// ============Retrieve Patients==========
+	@Override
+	public List<Patient> getByAdmittedDateBefore(LocalDate date) {
+		
+		List<Patient> patient = repo.findByAdmittedDateBefore(date);
+		patient.forEach(p->
+		System.out.println(p.getName()+" | "+p.getDisease()+" | "+p.getDoctorName())
+	);
+		return patient;
+	}
+
+	// ============Retrieve Patients==========
+	@Override
+	public List<Patient> getByAdmittedDateBetween(LocalDate start, LocalDate end) {
+		
+		List<Patient> patient = repo.findByAdmittedDateBetween(start, end);
+		patient.forEach(p->
+		System.out.println(p.getName()+" | "+p.getDisease()+" | "+p.getDoctorName())
+	);
+		return patient;
 	}
 
 }
